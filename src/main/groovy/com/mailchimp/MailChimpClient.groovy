3b1bd32d53aef74301ad16477e8adbc5c13d7bf9
Vijay <vshukla684@gmail.com>
2018-05-30 09:24:52 +0530

    RefactorCode

--- src/main/groovy/com/mailchimp/MailChimpClient.groovy
+++ src/main/groovy/com/mailchimp/MailChimpClient.groovy
@@ -14,115 +14,78 @@ import com.mailchimp.util.ObjectSerializer
 import static java.lang.String.format
 
 class MailChimpClient {
-    static String VERSION = "3.0"
-    static String SERVER_URL = "https://us18.api.mailchimp.com"
-
-    static String endpoint = "$SERVER_URL/$VERSION"
-
-    static String CAMPAIGN_FOLDER = "/campaign-folders"
-    static String CAMPAIGN = "/campaigns"
-    static String SINGLE_CAMPAIGN = "/campaigns/%s/"
-    static String CANCEL_CAMPAIGN = "/campaigns/%s/actions/cancel-send"
-    static String PAUSE_CAMPAIGN = "/campaigns/%s/actions/pause"
-    static String REPLICATE_CAMPAIGN = "/campaigns/%s/actions/replicate"
-    static String RESUME_CAMPAIGN = "/campaigns/%s/actions/resume"
-    static String SCHEDULE_CAMPAIGN = "/campaigns/%s/actions/schedule"
-    static String UNSCHEDULE_CAMPAIGN = "/campaigns/%s/actions/unschedule"
-    static String SEND_CAMPAIGN = "/campaigns/%s/actions/send"
-    static String CAMPAIGN_CONTENT = "/campaigns/%s/content"
-    static String CAMPAIGN_FEEDBACK = "/campaigns/%s/feedback"
-    static String CAMPAIGN_FEEDBACK_MESSAGE = "/campaigns/%s/feedback/%s"
-    static String CAMPAIGN_SEND_CHECKLIST = "/campaigns/%s/send-checklist"
-    static String SEARCH_CAMPAIGN = "/search-campaigns"
-
-    static String FILE_MANAGER_FILES = "/file-manager/files"
-    static String FILE_MANAGER_SPECIFIC = "/file-manager/files/%s"
-
-    static String LIST = "/lists"
-    static String LIST_SPECIFIC = "/lists/%s"
-    static String LIST_ACTIVITY = "/lists/%s/activity"
-    static String LIST_CLIENTS = "/lists/%s/clients"
-    static String LIST_LOCATIONS = "/lists/%s/locations"
-    static String LIST_MEMBERS = "/lists/%s/members"
-    static String LIST_SEGMENTS = "/lists/%s/segments"
-    static String LIST_WEBHOOKS = "/lists/%s/webhooks"
-    static String SEARCH_MEMBERS = "/search-members"
-
-    static String TEMPLATE_FOLDER = "/template-folders"
-    static String TEMPLATE_FOLDER_SINGLE = "/template-folders/%s"
-
-    static String TEMPLATE = "/templates"
-    static String TEMPLATE_SINGLE = "/templates/%s"
-    static String DEFAULT_TEMPLATE_SINGLE = "/templates/%s/default-content"
-
     Connection connection
     String apiKey
+    String serverUrl
+    String endpoint
 
     final ObjectSerializer objectSerializer = ObjectSerializer.INSTANCE
 
     MailChimpClient() {
-        this(null)
+        this(null, null)
     }
 
-    MailChimpClient(String apiKey) {
-        this(apiKey, new HttpClientConnection())
+    MailChimpClient(String apiKey, String serverUrl) {
+        this(apiKey, serverUrl, new HttpClientConnection())
     }
 
-    MailChimpClient(String apiKey, Connection connection) {
+    MailChimpClient(String apiKey, String serverUrl, Connection connection) {
         this.apiKey = apiKey
+        this.serverUrl = serverUrl
         this.connection = connection
+        this.endpoint = "${this.serverUrl}/${MailChimpURI.VERSION}"
     }
 
-    //Campaign API
     Campaign createCampaign(CampaignRequest request) {
-        return post(CAMPAIGN, request, Campaign.class)
+        return post(MailChimpURI.CAMPAIGN, request, Campaign.class)
     }
 
     ListResponse<Campaign> listAllCampaign() {
-        return list(CAMPAIGN, Campaign.class)
+        return list(MailChimpURI.CAMPAIGN, Campaign.class)
     }
 
     Campaign listCampaign(String campaignId) {
-        return getCall(format(SINGLE_CAMPAIGN, campaignId), Campaign.class)
+        return getCall(format(MailChimpURI.SINGLE_CAMPAIGN, campaignId), Campaign.class)
     }
 
     DeleteResponse deleteCampaign(String campaignId) {
-        return delete(format(SINGLE_CAMPAIGN, campaignId), DeleteResponse.class)
+        return delete(format(MailChimpURI.SINGLE_CAMPAIGN, campaignId), DeleteResponse.class)
     }
 
     Campaign updateCampaign(String campaignId, CampaignRequest request) {
-        return post(format(SINGLE_CAMPAIGN, campaignId), request, Campaign.class)
+        return post(format(MailChimpURI.SINGLE_CAMPAIGN, campaignId), request, Campaign.class)
     }
 
     void cancelSendCampaign(String campaignId, CampaignRequest request) {
-        post(format(CANCEL_CAMPAIGN, campaignId), request, Campaign.class)
+        post(format(MailChimpURI.CANCEL_CAMPAIGN, campaignId), request, Campaign.class)
     }
 
     void pauseCampaign(String campaignId, CampaignRequest request) {
-        post(format(PAUSE_CAMPAIGN, campaignId), request, Campaign.class)
+        post(format(MailChimpURI.PAUSE_CAMPAIGN, campaignId), request, Campaign.class)
     }
 
     void replicateCampaign(String campaignId, CampaignRequest request) {
-        post(format(REPLICATE_CAMPAIGN, campaignId), request, Campaign.class)
+        post(format(MailChimpURI.REPLICATE_CAMPAIGN, campaignId), request, Campaign.class)
     }
 
     void resumeCampaign(String campaignId, CampaignRequest request) {
-        post(format(RESUME_CAMPAIGN, campaignId), request, Campaign.class)
+        post(format(MailChimpURI.RESUME_CAMPAIGN, campaignId), request, Campaign.class)
     }
 
     void scheduleCampaign(String campaignId, CampaignRequest request) {
-        post(format(SCHEDULE_CAMPAIGN, campaignId), request, Campaign.class)
+        post(format(MailChimpURI.SCHEDULE_CAMPAIGN, campaignId), request, Campaign.class)
     }
 
     void sendCampaign(String campaignId, CampaignRequest request) {
-        post(format(SEND_CAMPAIGN, campaignId), request, Campaign.class)
+        post(format(MailChimpURI.SEND_CAMPAIGN, campaignId), request, Campaign.class)
     }
 
     void unScheduleCampaign(String campaignId, CampaignRequest request) {
-        post(format(UNSCHEDULE_CAMPAIGN, campaignId), request, Campaign.class)
+        post(format(MailChimpURI.UNSCHEDULE_CAMPAIGN, campaignId), request, Campaign.class)
     }
 
     protected <T> T getCall(String path, Class<T> responseClass) {
+        println(endpoint)
         return getCall(endpoint, path, responseClass)
     }
 
@@ -134,6 +97,7 @@ class MailChimpClient {
 
     protected <T> T post(String path, Object request, Class<T> responseClass) {
         String requestBody = objectSerializer.serialize(request)
+        println(endpoint)
         Response response = connection.post(endpoint + path, requestBody, buildHeaders())
         ensureSuccess(response)
         return objectSerializer.deserialize(response.body, responseClass)
--- src/main/groovy/com/mailchimp/sample/Test.groovy
+++ src/main/groovy/com/mailchimp/sample/Test.groovy
@@ -7,11 +7,13 @@ import com.mailchimp.request.SettingRequest
 
 class Test {
     public static void main(String[] args) {
+        String url = "https://us18.api.mailchimp.com"
+        String key = "bfbf396d994389360bb0a0b74feb1e34"
         RecipientRequest request = new RecipientRequest(listId: "3816c86404")
         SettingRequest settingRequest = new SettingRequest(subjectLine: "Your Purchase Receipt", replyTo: "mathur.akanksha2@gmail.com", fromName: "Akanksha mathur")
 
         CampaignRequest campaignRequest = new CampaignRequest(type: "regular", recipient: request, setting: settingRequest)
-        MailChimpClient client = new MailChimpClient("bfbf396d994389360bb0a0b74feb1e34")
+        MailChimpClient client = new MailChimpClient(key, url)
         println client.createCampaign(campaignRequest)
     }
 }
